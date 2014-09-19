7521c73dc55cecf80d8845091f97f25e9a101bda
Reda Lemeden <reda@thoughtbot.com>
2014-09-19 10:16:12 +0200

    Automated commit at 2014-09-19 08:16:12 UTC by middleman-deploy 0.2.4

---
+++ assets/javascripts/initializer-6bdf2f07.js
@@ -0,0 +1,1 @@
+(function(){var e;window.App={},hljs.initHighlightingOnLoad(),$(".js-disabled").removeClass("js-disabled").addClass("js-enabled"),$("data.wallet-id").on("click",function(){return e(event.target)}),e=function(e){var n,t;return t=window.getSelection(),n=document.createRange(),n.selectNodeContents(e),t.removeAllRanges(),t.addRange(n)}}).call(this);
